5eebe05cbfecc34e338a7c8621daf23f47747496
Alvaro Bustamante <varetax@gmail.com>
2015-07-21 23:25:28 -0300

    Vista inicio realizada, creacion de textos y listas
    Se crea la vista inicial de la aplicacion. Se configura el ListView en donde se mostraran las paginas de anime y
    el texto correspondiente.

--- app/src/main/java/com/ouiaboo/ouiaboo/MainActivity.java
+++ app/src/main/java/com/ouiaboo/ouiaboo/MainActivity.java
@@ -1,17 +1,40 @@
 package com.ouiaboo.ouiaboo;
 
+import android.app.ListActivity;
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ListActivity {
+
+    ListView paginasAnime;
+    ArrayAdapter<String> adaptador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String[] paginasWebNombre = getResources().getStringArray(R.array.paginas_anime);
+        String[] paginasWebIdioma = getResources().getStringArray(R.array.idioma_paginas_anime);
+
+        ArrayList<SitiosWeb> webList = new ArrayList<SitiosWeb>();
+
+        for (int i = 0; i < paginasWebNombre.length; i++){
+            webList.add(new SitiosWeb(paginasWebNombre[i], paginasWebIdioma[i]));
+        }
+
+        setListAdapter(new SitiosWebAdapter(this, R.layout.paginas_anime_lista, webList));
     }
 
     @Override
@@ -34,5 +57,73 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    //clase de las paginas web para ver anime
+    public class SitiosWeb{
+
+        private String nombre;
+        private String idioma;
+
+        public SitiosWeb(String nombre, String idioma){
+            this.nombre = nombre;
+            this.idioma = idioma;
+        }
+
+        public void setNombre(String nombre){
+            this.nombre = nombre;
+        }
+
+        public void setIdioma(String idioma){
+            this.idioma = idioma;
+        }
+
+        public String getNombre(){
+            return nombre;
+        }
+
+        public String getIdioma(){
+            return idioma;
+        }
+    }
+
+    public class SitiosWebAdapter extends ArrayAdapter<SitiosWeb>{
+        private ArrayList<SitiosWeb> items;
+        private SitiosWebViewHolder sitiosWebViewHolder;
+
+        private class SitiosWebViewHolder {
+            TextView nombre;
+            TextView idioma;
+        }
+
+        public SitiosWebAdapter(Context context, int layout, ArrayList<SitiosWeb> items){
+            super(context, layout, items);
+            this.items = items;
+        }
+
+        @Override
+        public View getView(int pos, View convertView, ViewGroup parent) {
+            View v = convertView;
+
+            if (v == null){
+                LayoutInflater vi = (LayoutInflater)getSystemService(LAYOUT_INFLATER_SERVICE);
+                v = vi.inflate(R.layout.paginas_anime_lista , null);
+                sitiosWebViewHolder = new SitiosWebViewHolder();
+                sitiosWebViewHolder.nombre = (TextView)v.findViewById(R.id.pagina);
+                sitiosWebViewHolder.idioma = (TextView)v.findViewById(R.id.idioma);
+                v.setTag(sitiosWebViewHolder);
+            } else {
+                sitiosWebViewHolder = (SitiosWebViewHolder)v.getTag();
+            }
+
+            SitiosWeb sitiosWeb = items.get(pos);
+
+            if (sitiosWeb != null){
+                sitiosWebViewHolder.nombre.setText(sitiosWeb.getNombre());
+                sitiosWebViewHolder.idioma.setText(sitiosWeb.getIdioma());
+            }
+
+            return v;
+        }
     }
 }
